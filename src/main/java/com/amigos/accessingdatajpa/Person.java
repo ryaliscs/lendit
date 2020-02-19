137a6efc9cadfe656eb619357b249e9c0ebfcbdd
Manoj Vutukuri <manoj.vutukuri@planonsoftware.com>
2020-02-19 21:23:39 +0530

    1. build.gradle with basic dependencies 2. Dummy class Person

---
+++ src/main/java/com/amigos/accessingdatajpa/Person.java
@@ -0,0 +1,13 @@
+package com.amigos.accessingdatajpa;
+
+public class Person {
+	private String name;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String aName) {
+		this.name = aName;
+	}
+}
