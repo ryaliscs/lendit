79190844ed537b199b4351399952e47227f1402c
Manoj Vutukuri <manoj.vutukuri@planonsoftware.com>
2020-02-22 22:28:33 +0530

    make application as spring boot application

---
+++ src/main/java/com/amigos/application/LenditApplication.java
@@ -0,0 +1,24 @@
+// Planon Enterprise Edition Source file: LenditApplication.java
+// Copyright Planon 1997-2020. All Rights Reserved.
+package com.amigos.application;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+/**
+ * LenditApplication
+ */
+@SpringBootApplication public class LenditApplication
+{
+  //~ Methods --------------------------------------------------------------------------------------
+
+  /**
+   * DOCUMENT ME!
+   *
+   * @param aArguments DOCUMENT ME!
+   */
+  public static void main(String[] aArguments)
+  {
+    SpringApplication.run(LenditApplication.class, aArguments);
+  }
+}
